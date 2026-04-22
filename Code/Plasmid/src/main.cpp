43ef17907e0f000e286df44cb6c89bbb6d7d448b
RS-212 <roman.soell@gmail.com>
2026-04-22 12:50:09 +0200

    test basics in pio

--- Code/Plasmid/src/main.cpp
+++ Code/Plasmid/src/main.cpp
@@ -4,15 +4,26 @@
 int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
+  // Serial connection to the PC
+  Serial0.begin(9600);
+  while(!Serial0);
+  delay(500);
+  Serial0.printf("Serial Monitor initialized\n");
+
+  // Pin setup
+  pinMode(LED_BUILTIN, OUTPUT);
+
   int result = myFunction(2, 3);
+
+  Serial0.printf("Fuction Test: %d\n", result);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Status Blinky
+  digitalWrite(BUILTIN_LED, !digitalRead(BUILTIN_LED));
+  delay(500);
 }
 
-// put function definitions here:
 int myFunction(int x, int y) {
   return x + y;
 }
